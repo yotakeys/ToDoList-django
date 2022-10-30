279fea700de5863a9e84d624f5e2fd45873af000
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-10-31 06:58:11 +0700

    Simple templating Read Task

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from.models import Task
 
 # Create your views here.
 
 
-def taskList(request):
-    return HttpResponse("To sod List")
+class TaskList(ListView):
+    model = Task
+    template_name = 'app/task_list.html'
+    context_object_name = "tasks"
