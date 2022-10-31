5f730190677fa8348c8d5b029ef474ebcebcf751
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-10-31 07:25:52 +0700

    Simple Template and routing Detail View

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from.models import Task
+from django.views.generic.detail import DetailView
+from .models import Task
 
 # Create your views here.
 
@@ -9,3 +10,9 @@ class TaskList(ListView):
     model = Task
     template_name = 'app/task_list.html'
     context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    Template_name = 'app/task_detail.html'
+    context_object_name = "task"
