8bd509cd9921f5f6b3f507426d94915695ff48fd
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-10-31 10:13:33 +0700

    Simple Template and Routing Create and Update Task

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='taskdetail')
+    path('taskdetail/<int:pk>/', TaskDetail.as_view(), name='taskdetail'),
+    path('taskcreate/', TaskCreate.as_view(), name='taskcreate'),
+    path('taskUpdate/<int:pk>/', TaskUpdate.as_view(), name='taskupdate'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
@@ -16,3 +18,17 @@ class TaskDetail(DetailView):
     model = Task
     Template_name = 'app/task_detail.html'
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'app/task_create.html'
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'app/task_update.html'
