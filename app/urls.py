0505462d20472897f1b2fa897321b468405b72f5
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-10-31 14:09:59 +0700

    Simple Template and routing Delete

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('taskdetail/<int:pk>/', TaskDetail.as_view(), name='taskdetail'),
     path('taskcreate/', TaskCreate.as_view(), name='taskcreate'),
-    path('taskUpdate/<int:pk>/', TaskUpdate.as_view(), name='taskupdate'),
+    path('taskupdate/<int:pk>/', TaskUpdate.as_view(), name='taskupdate'),
+    path('taskdelete/<int:pk>/', TaskDelete.as_view(), name='taskdelete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -32,3 +32,10 @@ class TaskUpdate(UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('tasks')
     template_name = 'app/task_update.html'
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    template_name = 'app/task_delete.html'
